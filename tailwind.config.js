4e1ab24ad6bbdb9e495e1627e60e3e81a27c96df
8kto <ifhtagn@gmail.com>
2024-07-26 00:58:20 +0200

    Adjust default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,ts}'],
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "retro",
+      {
+        emerald: {
+          ...require("daisyui/src/theming/themes")["emerald"],
+          "--rounded-btn": "0.25rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-box": "0.25rem",
+        },
+      },
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
   theme: {
     extend: {
       colors: {
